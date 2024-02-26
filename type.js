d8f57d5ffb30ed8c98155c546fe472554236ad6f
Subahan-1D <subahanislam523@gmail.com>
2024-02-26 13:53:39 +0600

    added by primitive data type

---
+++ type.js
@@ -0,0 +1,25 @@
+// Strongly typed language ;
+
+/*
+int a = 5
+string b = 'my name is subahan'
+boolean c = true ;
+object student = {name : 'subahan' , id : 677317 ;}
+
+*/
+
+// javaScript dynamic typed language 
+
+// Primitive data type
+
+const a = 5 ;
+const b = 'my name is Subahan Islam' ;
+const c = true ;
+ 
+// Non-Primitive Data Type
+
+const ages = [12,3,2,]
+const student = {id : 67 , class : 7}
+console.log(typeof a ,typeof b , typeof ages, typeof student);
+
+
